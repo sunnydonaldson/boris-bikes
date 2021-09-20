e79a515deffc036dfb389a7b3b7b541bffeb1c5d
twilliams1988 <tom@teow.co.uk>
2021-09-20 17:13:08 +0100

    add unit tests to check bike released and works

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,14 @@
 require "docking_station"
+require "bike"
 
 describe DockingStation do
 
   it { is_expected.to respond_to(:release_bike) }
+
+  it 'releases a working bike' do
+    docking_station = DockingStation.new
+    expect(docking_station.release_bike.class).to eq Bike
+    expect(docking_station.release_bike.working?).to eq true
+  end
 
 end
