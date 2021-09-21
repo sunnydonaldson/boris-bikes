9d38177fce3a4724d2f5ee3133733f81ec7a1bdf
Sunny Donaldson <sunnydonaldson1@gmail.com>
2021-09-21 15:14:34 +0100

    adding dock method and attr_reader bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,10 +1,21 @@
-require './lib/bike'
+require './bike'
 
 class DockingStation
+  attr_reader :bikes
+  def initialize
+    @bikes = []
+  end
 
   def release_bike
     Bike.new
   end
+
+  def dock(bike)
+    @bikes << bike
+    return "Bike docked!"
+  end
+
+
 
   
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,13 +2,37 @@ require "docking_station"
 require "bike"
 
 describe DockingStation do
+  bike = Bike.new
+  station = DockingStation.new
+  station.dock(bike)
+  station.dock(bike)
 
-  it { is_expected.to respond_to(:release_bike) }
+  describe "#release_bike" do
+    it { is_expected.to respond_to(:release_bike) }
 
-  it 'releases a working bike' do
-    docking_station = DockingStation.new
-    expect(docking_station.release_bike.class).to eq Bike
-    expect(docking_station.release_bike.working?).to eq true
+    it 'releases a working bike' do
+      docking_station = DockingStation.new
+      expect(docking_station.release_bike.class).to eq Bike
+      expect(docking_station.release_bike.working?).to eq true
+    end
   end
+
+  describe "#dock_bike" do
+
+    it "Allows users to store an instance of bike" do
+      #assert
+      expect(station.dock(bike)).to eq("Bike docked!")
+    end
+  end
+
+  describe "#bikes" do
+    it "allows users to see a list of bikes" do
+      expect(station.bikes).not_to be_empty
+    end
+
+  end
+
+
+
 
 end
